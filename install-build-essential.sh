ff67dbf2c1d634031515f448d48404fba31ec6b2
corretge <alex@corretge.cat>
2012-10-30 23:52:13 +0100

    Treiem fabric

--- install-build-essential.sh
+++ install-build-essential.sh
@@ -3,7 +3,7 @@
 apt-get update
 apt-get upgrade
 
-apt-get install -y git vim fabric build-essential
+apt-get install -y git vim build-essential
 
 apt-get install -y wget tree lynx dos2unix mc imagemagick mercurial
 
