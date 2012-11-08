a304a102b4c1599b10e71b334e3f308ef3af4638
corretge <alex@corretge.cat>
2012-11-08 11:49:56 +0100

    Add linux-headers- uname -r to install-build-essential

--- install-build-essential.sh
+++ install-build-essential.sh
@@ -3,7 +3,7 @@
 apt-get update
 apt-get upgrade
 
-apt-get install -y git vim build-essential
+apt-get install -y git vim build-essential linux-headers-`uname -r`
 
 apt-get install -y wget tree lynx dos2unix mc imagemagick mercurial
 
