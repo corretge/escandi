5b566fd3dd386c3e5e3fc3010a71ad28d5a05198
corretge <alex@corretge.cat>
2012-12-21 11:58:57 +0100

    add PhpDocumentor

---
+++ escandi.d/Alternatives/LAMPFromPackages.sh
@@ -0,0 +1,50 @@
+#!/bin/bash 
+
+
+##
+## currently a lot of PHP extension are missed in this repo.
+##
+##if [ ! -f /etc/apt/sources.list.d/ondrej-php5-precise.list ];
+##then
+##       add-apt-repository ppa:ondrej/php5
+##fi
+
+#apt-get update
+#apt-get upgrade -y
+
+apt-get install apache2-mpm-prefork -y
+
+apt-get install mysql-server-5.5 -y
+
+apt-get install php5 php5-dev php5-cli php5-curl php5-common php5-gd php5-sqlite php5-xmlrpc php5-xsl php5-mysqlnd php5-imagick php5-memcached php5-xdebug php-pear -y
+
+pear config-set php_ini /etc/php5/apache2/php.ini
+pear config-set auto_discover 1
+## Install APC Alternative PHP Cache
+if [ -n "`grep --quiet \"apc.so\" /etc/php5/apache2/php.ini`" ]; then
+	printf "\n" | pecl install apc
+fi
+
+## Install phing PHP Automating Build
+chkP=`pear info phing  | grep --quiet "No information found for"`
+if [ -n $chkP ]; then
+	pear install --alldeps pear.phpunit.de/PHPUnit
+	pear install --alldeps phing/phing
+fi
+
+## Install phpcs PHP Code Sniffer
+chkP=`pear info PHP_CodeSniffer  | grep --quiet "No information found for"`
+if [ -n $chkP ]; then
+	pear install --alldeps PHP_CodeSniffer
+fi
+
+## Install PHP Documentor
+chkP=`pear info PhpDocumentor  | grep --quiet "No information found for"`
+if [ -n $chkP ]; then
+	pear install --alldeps PhpDocumentor
+fi
+
+
+
+
+echo "OK"
