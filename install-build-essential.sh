ca8e19a0a57677d38094f3363ba17665754b2067
corretge <alex@corretge.cat>
2013-01-03 12:27:13 +0100

    add libpcre3-dev

--- install-build-essential.sh
+++ install-build-essential.sh
@@ -3,7 +3,7 @@
 apt-get update
 apt-get upgrade
 
-apt-get install -y git vim build-essential linux-headers-`uname -r` python-software-properties
+apt-get install -y git vim build-essential linux-headers-`uname -r` python-software-properties libpcre3-dev
 
 apt-get install -y wget tree lynx dos2unix mc imagemagick mercurial
 
