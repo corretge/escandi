044e99d56732c4c3a7f690d07bac6621631672b4
Àlex Corretgé <alex@corretge.cat>
2013-07-11 10:40:23 +0200

    add htop

--- install-build-essential.sh
+++ install-build-essential.sh
@@ -5,7 +5,5 @@ apt-get upgrade
 
 apt-get install -y git vim build-essential linux-headers-`uname -r` python-software-properties libpcre3-dev
 
-apt-get install -y wget tree lynx dos2unix mc imagemagick mercurial
-
-
+apt-get install -y wget tree lynx dos2unix mc imagemagick mercurial htop 
 
